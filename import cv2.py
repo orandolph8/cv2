4ca1d03158493da33e031a21da8d5985632d9cc1
orandolph8 <129277261+orandolph8@users.noreply.github.com>
2024-06-25 06:34:39 -0700

    Add files via upload

--- import cv2.py
+++ import cv2.py
@@ -2,18 +2,76 @@ import cv2
 import dlib
 import numpy as np
 
-img = cv2.imread(r'C:\Users\orand\OneDrive\Pictures\Pictures\1.jpg')
-img = cv2.resize(img, (0,0), None, 0.5, 0.5)
-imgOriginal = img.copy()
+webcam = True
 
+cap = cv2.VideoCapture(0)
 detector = dlib.get_frontal_face_detector()
-imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-faces = detector(imgGray)
-for face in faces:
-    x1, y1 = face.left(), face.top()
-    x2, y2 = face.right(),face.bottom()
-    imgOriginal = cv2.rectangle(img, (x1, y1), (x2,y2),(0,255,0),2)
+predictor = dlib.shape_predictor(r"C:\Users\orand\OneDrive\Pictures\Pictures\shape_predictor_68_face_landmarks.dat")
 
+def empty(a):
+    pass
+cv2.namedWindow('BGR')
+cv2.resizeWindow("BGR",640,240)
+cv2.createTrackbar('Blue','BGR',0,255,empty)
+cv2.createTrackbar('Green','BGR',0,255,empty)
+cv2.createTrackbar('Red','BGR',0,255,empty)
 
-cv2.imshow("Original", imgOriginal)
-cv2.waitKey(0)
+def createBox(img,points,scale=5, masked = False,cropped = True):
+    if masked:
+        mask = np.zeros_like(img)
+        mask = cv2.fillPoly(mask,[points],(255, 255, 255))
+        img = cv2.bitwise_and(img,mask)
+        cv2.imshow('Mask', img)
+    if cropped:
+        bbox = cv2.boundingRect(points)
+        x,y,w,h = bbox
+        imgCrop = img[y:y+h,x:x+w]
+        imgCrop = cv2.resize(imgCrop,(0,0),None,scale,scale)
+        return imgCrop
+    else:
+        return mask
+    
+while True:
+
+    if webcam: success , img = cap.read()
+    else: img = cv2.imread(r'OneDrive/Pictures/Pictures/1.webp')
+    img = cv2.resize(img, (0,0), None, 0.5, 0.5)
+    imgOriginal = img.copy()
+
+    imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = detector(imgGray)
+
+    for face in faces:
+        x1, y1 = face.left(), face.top()
+        x2, y2 = face.right(),face.bottom()
+        imgOriginal = cv2.rectangle(img, (x1, y1), (x2,y2),(0,255,0),2)
+        landmarks = predictor(imgGray,face)
+        myPoints = []
+        for n in range(68):
+            x = landmarks.part(n).x
+            y = landmarks.part(n).y
+            myPoints.append([x,y])
+            #cv2.circle(imgOriginal,(x,y),5,(50,50,255),cv2.FILLED)
+            #cv2.putText(imgOriginal,str(n),(x,y-10),cv2.FONT_HERSHEY_COMPLEX_SMALL,0.8,(0,0,255),1)
+        
+        myPoints = np.array(myPoints)
+        #imgLeftEye = createBox(img,myPoints[36:42])
+        imgLips = createBox(img,myPoints[48:61],3,masked=True,cropped=False)
+
+        imgColorLips = np.zeros_like(imgLips)
+        b = cv2.getTrackbarPos('Blue','BGR')
+        g = cv2.getTrackbarPos('Green','BGR')
+        r = cv2.getTrackbarPos('Red','BGR')
+        imgColorLips[:] = b,g,r
+        imgColorLips = cv2.bitwise_and(imgLips,imgColorLips)
+        imgColorLips = cv2.GaussianBlur(imgColorLips,(7,7),10)
+        imgOriginalGray = cv2.cvtColor(imgOriginal, cv2.COLOR_BGR2GRAY)
+        imgOriginalGray = cv2.cvtColor(imgOriginalGray, cv2.COLOR_GRAY2BGR)
+        imgColorLips = cv2.addWeighted(imgOriginal,1,imgColorLips,0.4,0)
+        cv2.imshow('BGR', imgColorLips)
+
+        cv2.imshow('Lips',imgLips)
+
+        print(myPoints)
+    cv2.imshow("Original", imgOriginal)
+    cv2.waitKey(1)
